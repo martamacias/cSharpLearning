2b4567165ba1e9d85e4281b0e4655a39b9ab2ad0
martamacias <program.martamacias@gmail.com>
2021-06-05 19:12:53 +0200

    first commit

---
+++ curso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
